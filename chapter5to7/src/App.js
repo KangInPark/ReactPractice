0862edd15b70a3572215fb14d139061676916c81
KangInPark <pkihome@naver.com>
2022-03-06 15:52:31 +0900

    Chapter6.2 Deps

--- chapter5to7/src/App.js
+++ chapter5to7/src/App.js
@@ -2,13 +2,25 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter((prev) => prev + 1);
-  console.log("i run all the time")
-  useEffect(()=>{
-    console.log("CALL THE API....")
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((x) => x + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API....");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5)
+      console.log("SEARCH FOR", keyword);
+  }, [keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
