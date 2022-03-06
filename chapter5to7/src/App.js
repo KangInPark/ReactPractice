273748d8e1b0898d68423b6a2c3a02835cf41c71
KangInPark <pkihome@naver.com>
2022-03-06 23:10:39 +0900

    Code Challange with Coin Tracker
    사용자가 가진 금액을 입력하여 선택한 코인을 몇개 구매 가능한지 표시

--- chapter5to7/src/App.js
+++ chapter5to7/src/App.js
@@ -3,6 +3,14 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [select, setSelect] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [selected, setSelected] = useState(false);
+  const onChange = (e) => {
+    setSelect(e.target.value.split(" "));
+    select[0] === "Select" ? setSelected(false) : setSelected(true);
+  };
+  const moneyChange = (e) => setMoney(e.target.value);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -17,7 +25,8 @@ function App() {
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={onChange}>
+          <option>Select Coin!</option>
           {coins.map((coin) => (
             <option key={coin.id}>
               {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
@@ -25,6 +34,16 @@ function App() {
           ))}
         </select>
       )}
+      <hr />
+      <label htmlFor="money">Your Money: </label>
+      <input
+        id="money"
+        placeholder="$"
+        value={money}
+        onChange={moneyChange}
+      ></input>
+      <br />
+      {selected ? `You can get ${money / select[2]} ${select[0]}!` : ""}
     </div>
   );
 }
