efe98adfb4228638f49b8bd0afb0d56f612a08ee
KangInPark <pkihome@naver.com>
2022-03-07 17:46:23 +0900

    Chapter7.6 Parameters + CodeChallange
    Detail 페이지 구현

--- chapter5to7/src/routes/Detail.js
+++ chapter5to7/src/routes/Detail.js
@@ -1,4 +1,46 @@
+import { Link, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import styles from "./Detail.module.css";
+
 function Detail() {
-	return <h1>Detail</h1>;
+	const { id } = useParams();
+	const [loading, setLoading] = useState(true);
+	const [movie, setMovie] = useState({});
+	const getDetail = async () => {
+		const json = await (
+			await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+		).json();
+		setMovie(json.data.movie);
+		setLoading(false);
+	};
+	useEffect(() => {
+		getDetail();
+	}, []);
+	return loading ? (
+		<h1>Loading...</h1>
+	) : (
+		<div>
+			<h1 className={styles.title}>{movie.title}</h1>
+			<img
+				src={movie.large_cover_image}
+				style={{
+					display: "block",
+					margin: "30px 5px",
+				}}
+			/>
+			<span className={styles.attr}>year: {movie.year}</span>
+			<span className={styles.attr}>rating: {movie.rating}</span>
+			<span className={styles.attr}>
+				download_count: {movie.download_count}
+			</span>
+			<span className={styles.attr}>like_count: {movie.like_count}</span>
+			<br />
+			<br />
+			<div className={styles.div}>{movie.description_full}</div>
+			<Link to="/">
+				<button className={styles.btn}>Return</button>
+			</Link>
+		</div>
+	);
 }
 export default Detail;
