beca6389252d9ff19610f18786ebc7aeb4214550
KangInPark <pkihome@naver.com>
2022-03-11 00:05:14 +0900

    수정: Home 화면 css 수정
    grid 및 Movie 컴포넌트 flex 적용.
    Movie 컴포넌트 꾸미기.
    스크롤 최상단으로 이동하는 Top버튼 추가.

---
+++ chapter5to7/src/components/Topbtn.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { FiArrowUpCircle } from "react-icons/fi";
+import { throttle } from "lodash";
+
+function Topbtn() {
+	const [scrolled, setScrolled] = useState(false);
+	const onClick = () => {
+		document.documentElement.scrollTop = 0;
+		setScrolled(false);
+	};
+	const scrollFunc = throttle(() => {
+		const val = window.scrollY;
+		val > 30 ? setScrolled(true) : setScrolled(false);
+	}, 100);
+	useEffect(() => {
+		window.addEventListener("scroll", scrollFunc);
+		return () => {
+			window.removeEventListener("scroll", scrollFunc);
+		};
+	}, [scrollFunc]);
+	return scrolled ? (
+		<FiArrowUpCircle
+			style={{
+				position: "fixed",
+				bottom: "1rem",
+				right: "1.9rem",
+				width: "50px",
+				height: "50px",
+				borderRadius: "100%",
+				backgroundColor: "white",
+			}}
+			onClick={onClick}
+		/>
+	) : null;
+}
+export default Topbtn;
--- chapter5to7/src/routes/Home.js
+++ chapter5to7/src/routes/Home.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
+import Topbtn from "../components/Topbtn";
 
 function Home() {
 	const [loading, setLoading] = useState(true);
@@ -19,7 +21,7 @@ function Home() {
 			{loading ? (
 				<h1>Loading...</h1>
 			) : (
-				<div>
+				<div className={styles.container}>
 					{movies.map((movie) => (
 						<Movie
 							key={movie.id}
@@ -32,6 +34,7 @@ function Home() {
 					))}
 				</div>
 			)}
+			<Topbtn />
 		</div>
 	);
 }
